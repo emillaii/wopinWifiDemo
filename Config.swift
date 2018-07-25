6f353797af090732fcf0a32ff020f5bb1e12af4e
emil <emillai@onwardsmg.com>
2018-07-25 20:22:47 +0800

    Initial Commit

---
+++ Config.swift
@@ -0,0 +1,11 @@
+//
+//  Config.swift
+//  WopinWifiTest
+//
+//  Created by Lai kwok tai on 7/7/2018.
+//  Copyright © 2018 Lai kwok tai. All rights reserved.
+//
+
+import Foundation
+
+let wopinURL = "http://172.16.0.1/wopin_wifi"
---
+++ WifiModule.swift
@@ -0,0 +1,39 @@
+//
+//  WifiModule.swift
+//  WopinWifiTest
+//
+//  Created by Lai kwok tai on 13/7/2018.
+//  Copyright © 2018 Lai kwok tai. All rights reserved.
+//
+
+import Foundation
+
+import NetworkExtension
+import SystemConfiguration.CaptiveNetwork
+
+struct WifiScanResult: Codable {
+    let essid: String
+    let bssid: String
+    let rssid: String?
+    let channel: String?
+    
+    private enum CodingKeys: String, CodingKey {
+        case essid = "essid"
+        case bssid = "bssid"
+        case rssid = "rssid"
+        case channel = "channel"
+    }
+}
+
+func getWifiSsid() -> String? {
+    var ssid: String?
+    if let interfaces = CNCopySupportedInterfaces() as NSArray? {
+        for interface in interfaces {
+            if let interfaceInfo = CNCopyCurrentNetworkInfo(interface as! CFString) as NSDictionary? {
+                ssid = interfaceInfo[kCNNetworkInfoKeySSID as String] as? String
+                break
+            }
+        }
+    }
+    return ssid
+}
